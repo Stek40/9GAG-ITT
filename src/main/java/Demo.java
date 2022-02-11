36ecebc249d164b4809b395eabfc4bffaf5e5f0f
martinLazarovv <96917357+martinLazarovv@users.noreply.github.com>
2022-02-11 19:52:20 +0200

    Delete src/main/java directory

--- src/main/java/Demo.java
+++
@@ -1,9 +0,0 @@
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("Hellsso");
-        System.out.println("Message from Stefan");
-
-        System.out.println("sth more");
-
-    }
-}
