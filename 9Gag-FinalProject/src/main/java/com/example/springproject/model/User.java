e2f08bbd0ecb8969d818a3a258096d4b2b2ce2b3
martinlazarovv <martin.lazarovv@abv.bg>
2022-02-11 19:48:32 +0200

    Martin added Spring Project

---
+++ 9Gag-FinalProject/src/main/java/com/example/springproject/model/User.java
@@ -0,0 +1,24 @@
+package com.example.springproject.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String username;
+    @Column
+    private String password;
+}
